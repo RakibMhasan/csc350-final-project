b1a32eb2d02c96e48f625d2381deec4e19b3a0a5
Mahamud Hasan <mahamud.hasan91@bmcc.cuny.edu>
2026-05-15 02:00:03 -0400

    Updated project criteria wording

--- judge_form.php
+++ judge_form.php
@@ -14,46 +14,96 @@ if (!isset($_SESSION["judge_id"])) {
 <head>
     <title>Judge Form</title>
 </head>
+
 <body>
+
     <h2>Computer Science Project Scoring</h2>
+
     <form method="post" action="submit_score.php">
-        Group Number: <input name="group_no" required><br><br>
-        Group Members: <input name="group_members" required><br><br>
-        Project Title: <input name="project_title" required><br><br>
+
+        Group Number:
+        <input name="group_no" required>
+        <br><br>
+
+        Group Members:
+        <input name="group_members" required>
+        <br><br>
+
+        Project Title:
+        <input name="project_title" required>
+        <br><br>
 
         <table border="1" cellpadding="10">
+
             <tr>
                 <th>Criteria</th>
                 <th>Developing (0-10)</th>
-                <th>Accomplished (10-15)</th>
+                <th>Accomplished (11-15)</th>
             </tr>
+
             <tr>
                 <td>Articulate requirements</td>
-                <td><input type="radio" name="c1" value="10" required></td>
-                <td><input type="radio" name="c1" value="15"></td>
+
+                <td>
+                    <input type="radio" name="c1" value="10" required>
+                </td>
+
+                <td>
+                    <input type="radio" name="c1" value="15">
+                </td>
             </tr>
+
             <tr>
-                <td>Design solution</td>
-                <td><input type="radio" name="c2" value="10" required></td>
-                <td><input type="radio" name="c2" value="15"></td>
+                <td>Choose appropriate tools and methods for each task</td>
+
+                <td>
+                    <input type="radio" name="c2" value="10" required>
+                </td>
+
+                <td>
+                    <input type="radio" name="c2" value="15">
+                </td>
             </tr>
+
             <tr>
-                <td>Write code</td>
-                <td><input type="radio" name="c3" value="10" required></td>
-                <td><input type="radio" name="c3" value="15"></td>
+                <td>Give clear and coherent oral presentation</td>
+
+                <td>
+                    <input type="radio" name="c3" value="10" required>
+                </td>
+
+                <td>
+                    <input type="radio" name="c3" value="15">
+                </td>
             </tr>
+
             <tr>
-                <td>Test and debug</td>
-                <td><input type="radio" name="c4" value="10" required></td>
-                <td><input type="radio" name="c4" value="15"></td>
+                <td>Functioned well as a team</td>
+
+                <td>
+                    <input type="radio" name="c4" value="10" required>
+                </td>
+
+                <td>
+                    <input type="radio" name="c4" value="15">
+                </td>
             </tr>
+
         </table>
 
         <br>
-        Judge Name: <input name="judge_name" required><br><br>
-        Comments: <input name="comments"><br><br>
+
+        Judge Name:
+        <input name="judge_name" required>
+        <br><br>
+
+        Comments:
+        <input name="comments">
+        <br><br>
 
         <input type="submit" value="Submit Score">
+
     </form>
+
 </body>
 </html>
